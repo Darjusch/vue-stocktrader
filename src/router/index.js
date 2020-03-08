a27996c76c17efd0b89dd8c0e9601639f5670939
Darjusch <Darjusch.schrand@code.berlin>
2020-03-08 14:04:34 +0100

    registration and login view

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/home/Home.vue'
 import Stocks from '@/components/stocks/Stocks.vue'
 import Portfolio from '@/components/portfolio/Portfolio.vue'
 import Info from '@/components/info/SymbolInfo.vue'
+import Register from '@/components/auth/Register.vue'
+import Login from '@/components/auth/Login.vue'
+
 
 Vue.use(Router)
 
 export const routes = [
-  { path: '/', name: 'Home', component: Home  },
   { path: '/stocks', name: 'Stocks', component: Stocks  },
   { path: '/portfolio', name: 'Portfolio', component: Portfolio },
-  { path: '/info/:symbolInfo', name: 'Info', component: Info, props: true}
+  { path: '/info/:symbolInfo', name: 'Info', component: Info, props: true},
+  { path: '/', name: 'Register', component: Register},
+  { path: '/login', name: 'Login', component: Login}
 ]
 
 const router = new Router({
